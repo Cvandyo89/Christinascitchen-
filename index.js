6d862c749eff9d6ed53ffcc2d9bd204007204bf4
Cvandyo89 <61753387+Cvandyo89@users.noreply.github.com>
2020-03-04 17:30:26 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var billAmount=89;
+function gratutity (billAmt) {
+    return billAmt *0.2
+}
+function totalwithGrat (billAmt){
+    var total=billAmt + gratutity(billAmt);
+    return total;
+}
+
+
+console.log(totalwithGrat(billAmount));
+console.log(totalwithGrat(95));
